d7eb11e5efadc8b03efe757023eaa8e0844e8e0b
ChrisAchinga <achinga.chris@gmail.com>
2021-06-19 22:31:14 +0300

    primitive types

---
+++ primitive-types/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace primitive_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    byte number = 23;
+        //    int count = 5;
+        //    float totalPrice = 33.23f;
+        //    char character = 'Q';
+        //    string name = "Chris";
+        //    bool isActive = false;
+
+           var number = 23;
+           var count = 5;
+           var totalPrice = 33.23f;
+           var character = 'Q';
+           var name = "Chris";
+           var isActive = false;
+
+           Console.WriteLine(number);
+           Console.WriteLine(count);
+           Console.WriteLine(totalPrice);
+           Console.WriteLine(character);
+           Console.WriteLine(name);
+           Console.WriteLine(isActive);
+        }
+    }
+}
