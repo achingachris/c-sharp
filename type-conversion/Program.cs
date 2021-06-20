a3a1a8f79ea65c88039927290a03e7b700f40f45
ChrisAchinga <achinga.chris@gmail.com>
2021-06-20 22:44:47 +0300

    type conversions

---
+++ type-conversion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace type_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // byte b =2;
+            // int i = b;
+            // Console.WriteLine(i);
+
+            // int i = 255;
+            // byte b = (byte) i;
+
+            // Console.WriteLine(b);
+
+            // var number = "12345";
+            // int i = Convert.ToInt32(number);
+
+            // Console.WriteLine(i);
+
+            try
+            {
+                
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Conversion Failed");
+            }
+
+
+            try
+            {
+               string str = "true";
+               bool b = Convert.ToBoolean(str);
+               Console.WriteLine(b);
+            }
+            catch (System.Exception)
+            {
+                
+                Console.WriteLine("Conversion Failed");
+            }
+
+        }
+    }
+}
