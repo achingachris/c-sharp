6707aa24cd1876f405fd792e8c5cb0c98d483ee2
ChrisAchinga <achinga.chris@gmail.com>
2021-06-26 21:15:08 +0300

    apps

---
+++ app/Console/user-input/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
