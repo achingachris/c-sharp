60736328cb36fd157d2bd1a41d62e305ecdaa884
ChrisAchinga <achinga.chris@gmail.com>
2021-06-29 09:47:29 +0300

    csharp classes

---
+++ class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace classdemo
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Person();
+            student1.firstName = "Habieber";
+            student1.lastName = "Kadhi";
+            student1.Introduce();
+        }
+    }
+}
