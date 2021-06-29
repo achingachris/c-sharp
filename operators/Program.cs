fd9afd7bec8fe5d899d32e3293182348f78bd4f3
ChrisAchinga <achinga.chris@gmail.com>
2021-06-29 09:21:33 +0300

    operators

---
+++ operators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            var d = 10;
+
+            // normal operations
+            Console.WriteLine(d / c);
+
+            // getting results in floating point numbers
+            Console.WriteLine("floating point number");
+            Console.WriteLine((float)d / (float)c);
+
+            // using operator precedence
+            Console.WriteLine("opeartor precedence");
+            Console.WriteLine(a + b * c);
+            Console.WriteLine((a + b) * c);
+
+            // using comparison operators
+            Console.WriteLine("Comparison Operators");
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b));
+
+            // logical operators
+            Console.WriteLine("Logical Operators");
+            Console.WriteLine(c > b && c > a);
+            Console.WriteLine(c > b && c == a);
+            Console.WriteLine(c > b || c == a);
+        }
+    }
+}
