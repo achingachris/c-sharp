0d54e084d6ebed890b8cff26528e06d5df9e5dbf
ChrisAchinga <achinga.chris@gmail.com>
2021-06-29 11:50:24 +0300

    strings

---
+++ string/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace stringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Habieber";
+            var lastName = "Kadhi";
+
+            var fullName = firstName + " " + lastName;
+
+            var crushName = string.Format("My crush is {0} {1}", firstName, lastName);
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(crushName);
+
+            var countries = new string[3] {"Switzerland", "German", "France"};
+            var allCountries = string.Join(",", countries);
+            Console.WriteLine(allCountries);
+        }
+    }
+}
