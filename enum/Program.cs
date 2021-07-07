7b217a0a861873787914ddba8c917346bd3ea633
ChrisAchinga <achinga.chris@gmail.com>
2021-07-07 14:01:52 +0300

    /

---
+++ enum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace enumDemo
+{
+    public enum travelAir
+    {
+        economy = 12,
+        premiumEconomy = 10,
+        business = 5
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = travelAir.business;
+            Console.WriteLine((int)method);
+        }
+    }
+}
---
+++ user-input/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace user_input
+{
+    class Test
+	{
+		public static void Main(string[] args)
+		{
+			string name;
+			Console.WriteLine("What is your name?");
+			name = Console.ReadLine();
+			Console.WriteLine("Hello {0}, Welcome to the console", name);
+		}
+	}
+}
