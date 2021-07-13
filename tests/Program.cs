4e7a603664f604cda1a08b9dd5989d17309cbd97
ChrisAchinga <achinga.chris@gmail.com>
2021-07-13 12:26:11 +0300

    test runs

---
+++ tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime().Now().Year();
+
+
+            Console.WriteLine(dateTime.Year);
+        }
+    }
+}
